59ae949795e7949238030afb0a80e79c63bc0770
Anthony Generalov <anthony.weres.force@gmail.com>
2020-03-25 10:57:29 +0200

    Update

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const requestHandler = require('./myRoutes');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(requestHandler);
+const app = express();
 
-server.listen(3555);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use('/',(req, res, next)=>{
+    res.status(404);
+    res.sendFile(path.join(__dirname,'views','PageNotFound.html'));
+});
+
+app.listen(3555);
---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    console.log('I am another middleware');    
+    res.sendFile(path.join(__dirname,'..','views','shop.html'));
+
+    
+});
+
+
+module.exports = router;
+
+
