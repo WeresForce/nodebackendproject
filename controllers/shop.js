44fd5b8b8990b23c8880e4b0efea9ad80dc93adc
Anthony <anthony.weres.force@gmail.com>
2020-04-10 18:13:24 +0300

    Routes update

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,6 +20,12 @@ exports.getProducts = (req,res,next)=>{
     });  
 };
 
+exports.getProduct = (req,res,next) =>{
+    const prodId = req.params.productId;
+    console.log(prodId);
+    res.redirect('/');
+};
+
 exports.getCart = (req,res, next) =>{
     res.render('shop/cart',{
                             pageTitle:'Your Cart',
