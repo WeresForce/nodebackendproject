d6ef372b1b20bc3a7ce7ae9ef3c6f2b79432253c
Anthony Generalov <anthony.weres.force@gmail.com>
2020-04-25 14:39:38 +0300

    upd

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const rootDir = require('./utils/path');
 const sequilize = require('./utils/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -36,6 +38,10 @@ app.use(errorController.getPageNotFound);
 
 Product.belongsTo(User,{ constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 sequilize
 //{force: true} forces recreate
@@ -48,6 +54,9 @@ sequilize
         return User.create({name: 'FirstUser', email:'test@test.com'});
     }
     return Promise.resolve(user);
+})
+.then(user =>{
+    return user.createCart();
 })
 .then( user =>{
     app.listen(3555);
--- models/cart-item.js
+++ models/cart-item.js
@@ -3,13 +3,16 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 
 const sequelize = require('../utils/database');
 
-const Cart = sequelize.define('cart',{
+const CartItem = sequelize.define('cartItem',{
     id:{
         type: DataTypes.INTEGER,
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
+    },
+    quantity: {
+        type: DataTypes.INTEGER
     }
 });
 
-module.exports = Cart;
+module.exports = CartItem;
